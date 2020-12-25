6eec1aaf6af3f38d1aae271a0bd4123385465c0b
Ravi Tripathi <ravi.tripathi@go-jek.com>
2020-12-25 20:10:31 +0530

    Initial Commit

---
+++ Arcadia/ArcadiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArcadiaApp.swift
+//  Arcadia
+//
+//  Created by Ravi Tripathi on 25/12/20.
+//
+
+import SwiftUI
+
+@main
+struct ArcadiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Arcadia/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Arcadia
+//
+//  Created by Ravi Tripathi on 25/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
