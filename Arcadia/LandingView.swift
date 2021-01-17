68ca0cb3620c8e0c006d8e8ba80478ce39287c82
Ravi Tripathi <ravitripathi1996@gmail.com>
2021-01-17 23:55:41 +0530

    Updated Eyetracking

--- Arcadia/LandingView.swift
+++ Arcadia/LandingView.swift
@@ -7,17 +7,24 @@
 
 import SwiftUI
 import GitBrowser
+import EyeTracker
 
 struct LandingView: View {
     var body: some View {
         NavigationView {
             List {
-                    NavigationLink(
-                        destination:
-                            GitBrowser.ContentView().environmentObject(NetworkStore()),
-                        label: {
-                            Text("GitBrowser")
-                        })
+                NavigationLink(
+                    destination:
+                        GitBrowser.ContentView().environmentObject(NetworkStore()),
+                    label: {
+                        Text("GitBrowser")
+                    })
+                NavigationLink(
+                    destination:
+                        EyeTracker.EyeTrackerView(),
+                    label: {
+                        Text("EyeTracker")
+                    })
                 
             }.navigationBarTitle(Text("Arcadia"))
         }
