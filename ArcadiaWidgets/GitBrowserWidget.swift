d2a6da43784843bd613d21f48e260d785c51404c
Ravi Tripathi <ravitripathi1996@gmail.com>
2021-08-08 18:56:28 +0530

    Removed xcodeproj references for Arcadia workspace

---
+++ ArcadiaWidgets/GitBrowserWidget.swift
@@ -0,0 +1,150 @@
+//
+//  GitBrowserWidget.swift
+//  GitBrowserWidget
+//
+//  Created by Ravi Tripathi on 25/07/20.
+//
+
+import WidgetKit
+import SwiftUI
+import Intents
+import GitBrowser
+
+// Struct for data in widget
+struct GithubUserEntry: TimelineEntry {
+    let date: Date
+    let userData: User
+    let imageData: Data
+}
+
+// Dynamic, confiurable widget
+// Contains logic for updating/viewing the widget
+struct GitBrowserIntentTimelineProvider: IntentTimelineProvider {
+    func placeholder(in context: Context) -> GithubUserEntry {
+        let user = User()
+        user.name = "Here's one dummy user"
+        user.login = "@userloginhere"
+        let imgData = UIImage(systemName: "person")!.pngData()!
+        return GithubUserEntry(date: Date(), userData: user, imageData: imgData)
+    }
+    
+    typealias Entry = GithubUserEntry
+    
+    typealias Intent = GitBrowserIntentIntent
+    
+    func getSnapshot(for configuration: GitBrowserIntentIntent, in context: Context, completion: @escaping (GithubUserEntry) -> Void) {
+        let user = User()
+        user.name = "Here's one dummy user"
+        user.login = "@userloginhere"
+        let imgData = UIImage(systemName: "person")!.pngData()!
+        let entry = GithubUserEntry(date: Date(), userData: user, imageData: imgData)
+        completion(entry)
+    }
+    
+    func getTimeline(for configuration: GitBrowserIntentIntent, in context: Context, completion: @escaping (Timeline<GithubUserEntry>) -> Void) {
+        
+        var username = AppData.lastCurrentUsername
+        if let u = configuration.username, !u.isEmpty {
+            username = u
+        }
+        NetworkManager.shared.getUserDetails(forUsername: username) { (user) -> (Void) in
+            let dummyUser = User()
+            dummyUser.name = "Here's one dummy user"
+            dummyUser.login = "@userloginhere"
+            let imgData = UIImage(systemName: "person")!.pngData()!
+            let emptyTimeLine = Timeline(entries: [GithubUserEntry(date: Date(), userData: dummyUser, imageData: imgData)], policy: .atEnd)
+            guard let user = user, let userImageUrlS = user.avatar_url, let userImageUrl = URL(string: userImageUrlS) else {
+                completion(emptyTimeLine)
+                return
+            }
+            
+            URLSession.shared.dataTask(with: userImageUrl) { (data, resp, error) in
+                guard let data = data else {
+                    completion(emptyTimeLine)
+                    return
+                }
+                let entry = GithubUserEntry(date: Date(), userData: user, imageData: data)
+                //Policy decides when to reload the widget. never -> remains fixed
+                let timeline = Timeline(entries: [entry], policy: .atEnd)
+                completion(timeline)
+            }.resume()
+        }
+    }
+}
+
+//What the widget should look like
+struct WidgetEntryView: View {
+    
+    let entry: GitBrowserIntentTimelineProvider.Entry
+    
+    @Environment(\.widgetFamily) var family
+    
+    @ViewBuilder //Allows adding switch statements to the view
+    var body: some View {
+        switch family {
+        default:
+            MediumWidget(user: entry.userData, imageData: entry.imageData)
+        }
+        
+    }
+}
+
+public struct GitBrowserWidget: Widget {
+    
+    private let kind = "GitBrowserWidget"
+    
+    public init() {}
+    
+    public var body: some WidgetConfiguration {
+        
+        IntentConfiguration(kind: kind, intent: GitBrowserIntentIntent.self, provider: GitBrowserIntentTimelineProvider()) { entry in
+            WidgetEntryView(entry: entry)
+        }.supportedFamilies([.systemMedium])
+        .configurationDisplayName("GitBrowser")
+        .description("Display GitHub user data")
+
+        //        Use this to set what sizes of widgets you want to support. Also works without it
+        
+        //        Building static widgets
+        //        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+        //            WidgetEntryView(entry: entry)
+        //        }
+        //        .supportedFamilies([.systemMedium])
+    }
+}
+
+//Static Widget Timeline
+//Contains logic for updating/viewing the widget
+//struct Provider: IntentTimelineProvider {
+//
+////    typealias Entry = GithubUserEntry
+//    typealias Intent = GithubUserEntry
+//
+//    //Renders snapshot on home page. Dummy data. Fast returning
+//    func getSnapshot(for configuration: Intent, in context: Context, completion: @escaping (GithubUserEntry) -> Void) {
+//        let entry = GithubUserEntry(date: Date(), userData: User(), imageData: UIImage(systemName: "person")!.pngData()!)
+//        completion(entry)
+//    }
+//
+//
+//    func getTimeline(for configuration: Intent, in context: Context, completion: @escaping (Timeline<GithubUserEntry>) -> Void) {
+//        NetworkManager.shared.getUserDetails(forUsername: AppData.lastCurrentUsername) { (user) -> (Void) in
+//            let emptyTimeLine = Timeline(entries: [GithubUserEntry(date: Date(), userData: User(), imageData: Data())], policy: .atEnd)
+//            guard let user = user, let userImageUrlS = user.avatar_url, let userImageUrl = URL(string: userImageUrlS) else {
+//                completion(emptyTimeLine)
+//                return
+//            }
+//
+//            URLSession.shared.dataTask(with: userImageUrl) { (data, resp, error) in
+//                guard let data = data else {
+//                    completion(emptyTimeLine)
+//                    return
+//                }
+//                let entry = GithubUserEntry(date: Date(), userData: user, imageData: data)
+//                //Policy decides when to reload the widget. never -> remains fixed
+//                let timeline = Timeline(entries: [entry], policy: .atEnd)
+//                completion(timeline)
+//            }.resume()
+//        }
+//    }
+// }
---
+++ ArcadiaWidgets/MediumWidget.swift
@@ -0,0 +1,61 @@
+//
+//  MediumWidget.swift
+//  GitBrowser (iOS)
+//
+//  Created by Ravi Tripathi on 26/07/20.
+//
+
+import SwiftUI
+import WidgetKit
+import Kingfisher
+import GitBrowser
+
+struct MediumWidget: View {
+    
+    @State var user: User
+    @State var imageData: Data
+    
+    var body: some View {
+        HStack {
+            Image(uiImage: UIImage(data: imageData)!)
+                .resizable()
+                .frame(width: 100.0, height: 100.0)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                .shadow(radius: 10)
+            
+            VStack(alignment: .leading) {
+                Text(user.name ?? "")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Text("@\(user.login ?? "")")
+                    .font(.title2)
+                Text("Followers: \(user.followers ?? 0)")
+                Text("Followers: \(user.following ?? 0)")
+                HStack {
+                    Image(systemName: "location.fill")
+                    Text("\(user.location ?? "Unknown")")
+                }
+            }
+        }.padding()
+    }
+}
+
+struct MediumWidget_Previews: PreviewProvider {
+    
+    static var dummyUser: User = {
+        let user = User()
+        user.name = "John Doe"
+        user.login = "johndoe"
+        user.company = "Company brrrr"
+        user.avatar_url = "https://github.com/onevcat/Kingfisher/blob/master/images/kingfisher-1.jpg?raw=true"
+        return user
+    }()
+    
+    static var previews: some View {
+        Group {
+            MediumWidget(user: MediumWidget_Previews.dummyUser, imageData: UIImage(systemName: "person")!.pngData()!)
+                .previewContext(WidgetPreviewContext(family: .systemMedium))
+        }
+    }
+}
